fa8050fb5687a6c3fd8b42efe98d6ea0c0724290
Dan Pittman <dan@dpitt.me>
2025-05-02 16:20:24 -0700

    this is a functional keccak impl but the indices are mixed up

--- src/Keccak.hs
+++ src/Keccak.hs
@@ -7,18 +7,16 @@ import Data.Word
 
 import Util hiding (matrify)
 
-import Debug.Trace
-
 -- | XOR sheets, where a sheet is the nth item in each inner
 -- | array.
-theta1_ :: (Bits w, Num w) => [[w]] -> [w]
+theta1_ :: [[Word64]] -> [Word64]
 theta1_ state
   | length state /= 5 = unreachable
   | all (\x -> length x /= 5) state = unreachable
   | otherwise = map (foldr xor 0) (transpose state)
 
 -- | XOR neighbors of the current index.
-theta2_ :: (Bits w) => [w] -> [w]
+theta2_ :: [Word64] -> [Word64]
 theta2_ cols
   | length cols /= 5 = unreachable
   | otherwise = go cols 0
@@ -31,58 +29,59 @@ theta2_ cols
 
 -- | XOR each inner-array element with the columns we've been working
 -- | on.
-theta3_ :: (Bits w) => [[w]] -> [w] -> [[w]]
+theta3_ :: [[Word64]] -> [Word64] -> [[Word64]]
 theta3_ is cols = map (zipWith xor cols) is
 
-theta :: (Bits w, Num w) => [[w]] -> [[w]]
+theta :: [[Word64]] -> [[Word64]]
 theta state = theta3_ state $ theta2_ $ theta1_ state
 
-offset_ :: (Integral w) => w -> w -> w
+offset_ :: Int -> Int -> Int
 offset_ w t = ((t + 1) * (t + 2) `div` 2) `mod` w
 
 coords_ :: Int -> Int -> (Int, Int)
 coords_ x y = (y, ((2 * x) + (3 * y)) `mod` 5)
 
 coordsToOffsets_ :: Int -> [Int]
-coordsToOffsets_ w = 0 : map snd (sortOn fst (go (1, 0) 0))
+coordsToOffsets_ w = 0 : map snd (sortOn fst (map swap (gen (1, 0) 0)))
   where
-    go _ 24 = []
-    go (x, y) i = ((x, y), offset_ w i) : go (coords_ x y) (i + 1) 
+    gen _ 24 = []
+    gen (x, y) i = ((x, y), offset_ w i) : gen (coords_ x y) (i + 1)
+    swap ((x, y), o) = ((y, x), o)
 
-rho :: (Bits w, Integral w) => Int -> [[w]] -> [[w]]
+rho :: Int -> [[Word64]] -> [[Word64]]
 rho width state =
-  transpose . matrify $ zipWith rotateL (concat state) (coordsToOffsets_ width) 
+  matrify $ zipWith rotateL (concat state) (coordsToOffsets_ width) 
                 
 
-pi :: [[w]] -> [[w]]
+pi :: [[Word64]] -> [[Word64]]
 pi [] = []
 pi state = go 0
   where
     go 5 = []
     go i =
-      [ state !! (mkX 0 i) !! 0
-      , state !! (mkX 1 i) !! 1
-      , state !! (mkX 2 i) !! 2
-      , state !! (mkX 3 i) !! 3
-      , state !! (mkX 4 i) !! 4
+      [ state !! 0 !! (mkX 0 i)
+      , state !! 1 !! (mkX 1 i)
+      , state !! 2 !! (mkX 2 i)
+      , state !! 3 !! (mkX 3 i)
+      , state !! 4 !! (mkX 4 i)
       ]
         : go (i + 1)
     mkX x y = (x + (3 * y)) `mod` 5
 
-chiTrans_ :: (Bits w) => [[w]] -> Int -> Int -> w
+chiTrans_ :: [[Word64]] -> Int -> Int -> Word64
 chiTrans_ state x y =
-  let a = state !! x !! y
-      a1 = state !! ((x + 1) `mod` 5) !! y
-      a2 = state !! ((x + 2) `mod` 5) !! y
+  let a = state !! y !! x
+      a1 = state !! y !! ((x + 1) `mod` 5)
+      a2 = state !! y !! ((x + 2) `mod` 5)
    in a `xor` (complement a1 .&. a2)
 
-chi :: (Bits w) => [[w]] -> [[w]]
+chi :: [[Word64]] -> [[Word64]]
 chi state = outer state 0
   where
     outer _ 5 = []
     outer st x = inner st x 0 : outer st (x + 1)
     inner _ _ 5 = []
-    inner st' x y = chiTrans_ st' x y : inner st' x (y + 1)
+    inner st' x y = chiTrans_ st' y x : inner st' x (y + 1)
 
   
 lfsr_ :: Int -> Word8 -> Bool
@@ -99,7 +98,7 @@ rc_ count
   | count == 0 = True
   | otherwise = lfsr_ count 1
 
-mkRC_ :: (Bits w, Num w) => Int -> Int -> w -> w
+mkRC_ :: Int -> Int -> Word64 -> Word64
 mkRC_ log2 rd rc = go 0 rd rc
   where
     go j r rc
@@ -111,24 +110,24 @@ mkRC_ log2 rd rc = go 0 rd rc
                 else rc
          in go (j + 1) r rc'
   
-iota :: (Bits w, Num w, Show w) => Int -> Int -> [[w]] -> [[w]]
+iota :: Int -> Int -> [[Word64]] -> [[Word64]]
 iota log2 round ((i:inner):outer) = (i `xor` mkRC_ log2 round 0 : inner) : outer
 iota _ _ _ = unreachable
 
-rnd :: (Bits w, Num w, Integral w, Show w) => Int -> Int -> Int -> [[w]] -> [[w]]
+rnd :: Int -> Int -> Int -> [[Word64]] -> [[Word64]]
 rnd l width i = iota l i . chi . pi . rho width . theta
 
 matrify :: [w] -> [[w]]
-matrify s = transpose (go s)
+matrify s = (go s)
   where
     go [] = []
     go (s1:s2:s3:s4:s5:ss) = [s1, s2, s3, s4, s5] : go ss
     go _ = unreachable
 
-unmatrify :: [[w]] -> [w]
-unmatrify s = concat (transpose s)
+unmatrify :: [[Word64]] -> [Word64]
+unmatrify s = concat (s)
 
-keccak_f :: (Bits w, Num w, Integral w, Show w) => Int -> Int -> [w] -> [w]
+keccak_f :: Int -> Int -> [Word64] -> [Word64]
 keccak_f l width input = unmatrify $ run 0 (matrify input)
   where
     run r s
